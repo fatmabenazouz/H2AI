c16a54a4b104f44d526baba6063a382815930715
Esther Umunyana <e.umunyana@alustudent.com>
2023-12-07 23:38:00 +0200

    Made some changes to the footer and the About-us section

---
+++ main.js
@@ -0,0 +1,42 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    const starContainer = document.getElementById("starContainer");
+
+    // Number of stars
+    const totalStars = 5;
+
+    // Initialize the star rating
+    let currentRating = 0;
+
+    // Generate star icons
+    for (let i = 1; i <= totalStars; i++) {
+        const star = document.createElement("i");
+        star.classList.add("fas", "fa-star");
+        star.dataset.ratingValue = i;
+
+        // Add a click event listener to each star
+        star.addEventListener("click", function () {
+            currentRating = parseInt(star.dataset.ratingValue);
+            updateStarRating();
+        });
+
+        starContainer.appendChild(star);
+    }
+
+    // Function to update star rating based on user interaction
+    function updateStarRating() {
+        // Reset all stars
+        starContainer.querySelectorAll("i").forEach(star => {
+            star.classList.remove("fas", "far");
+            star.classList.add("far");
+        });
+
+        // Fill selected stars
+        for (let i = 1; i <= currentRating; i++) {
+            starContainer.querySelector(`[data-rating-value="${i}"]`).classList.add("fas");
+        }
+    }
+});
+
+
